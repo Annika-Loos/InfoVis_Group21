556d3088dbb4a8aadfba62b3a74eb293c3744bd5
Kyzer Gerez <kyzer.gerez@gmail.com>
2025-01-14 23:21:29 +0100

    barplot

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // make each data point a button
                 svg.select("#" + elementID)
                     .on("click", function() {
-                        updateBar(records);
+                        updateBar(artist, year, records);
                     });
                 });
         });
@@ -169,17 +169,57 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Function to update bar chart
-    function updateBar(records) {
+    function updateBar(artist, year, records) {
+        // Clear the existing visualization
+        d3.select("#visualization_bar").html("");
+
+        document.getElementById("barTitle").textContent = artist + " (" + year + ")";
+
         // initialization
         const margin = { top: 20, right: 30, bottom: 50, left: 150 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
 
-        // const svg = d3.select("#visualization_bar")
-        //     .append("svg")
-        //     .attr("width", width + margin.left + margin.right)
-        //     .attr("height", height + margin.top + margin.bottom)
-        //     .append("g")
-        //     .attr("transform", `translate(${margin.left},${margin.top})`);
+        // Preprocess data
+        const parsed = d3.rollups(records, v => v.length, d => d.country);
+        const countries = parsed.map(d => d[0]);
+        const exhibitions = parsed.map(d => d[1]);
+
+        const svg = d3.select("#visualization_bar")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        // Scales
+        const x = d3.scaleBand()
+                .domain(countries)
+                .range([0, width])
+                .padding(0.1);
+
+        const y = d3.scaleLinear()
+                .domain([0, d3.max(exhibitions)])
+                .range([height, 0]);
+
+        // Axes
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x));
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Create bars
+        svg.selectAll(".bar")
+            .data(parsed)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => x(d[0]))
+            .attr("y", d => y(d[1]))
+            .attr("width", x.bandwidth())
+            .attr("height", d => height - y(d[1]))
+            .attr("fill", "steelblue");
     }
 });
